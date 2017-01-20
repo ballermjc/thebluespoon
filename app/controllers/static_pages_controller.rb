bd6420886a7e7071cf819479c3cc0e5b8a08b298
Michael J Cooper <ballermjc@gmail.com>
2017-01-20 13:13:53 -0700

    Email Form Feature

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -11,5 +11,15 @@ class StaticPagesController < ApplicationController
 
   def contact
   end
-  
+
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    ActionMailer::Base.mail(:from => @email,
+      :to => 'ballermjc@gmail.com',
+      :subject => "A new contact form message from #{@name}",
+      :body => @message).deliver_now
+  end
+
 end
