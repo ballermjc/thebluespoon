a7710037f271c30ee3d94daa1edebf871ff72b54
Michael J Cooper <ballermjc@gmail.com>
2017-01-31 21:38:30 -0700

    production issue fixed

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,11 +5,10 @@ class Ability
 
     user ||= User.new #guest user (not logged in)
     can :manage, User, id: user.id
-  end
 
-  if user.admin?
-    can :destroy, Comment
-    can :manage, Product
+    if user.admin?
+      can :destroy, Comment
+      can :manage, Product
+    end
   end
-  
 end
