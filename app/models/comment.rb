71d236bd3432b8641d740a2df1f48bc7dd1f0388
Michael J Cooper <ballermjc@gmail.com>
2017-02-15 20:21:58 -0700

    6.14 ActiveJob and live stream comments to logged in users

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,8 +1,9 @@
 class Comment < ApplicationRecord
   validates :body, presence: true
-    validates :user, presence: true
-    validates :product, presence: true
-    validates :rating, numericality: { only_integer: true }
+  validates :user, presence: true
+  validates :product, presence: true
+  validates :rating, numericality: { only_integer: true }
+  after_create_commit { CommentUpdateJob.perform_later(self, @user) }
   belongs_to :user
   belongs_to :product
 
